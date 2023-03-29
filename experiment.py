7054d4dde01d1c1c3200d47adfb2437575a6d364
declan <declan@pop-os.localdomain>
2023-03-29 19:08:15 -0400

    Fixed scanner start issue.

--- experiment.py
+++ experiment.py
@@ -4,6 +4,7 @@ from glob import glob
 from PIL import Image
 from psychopy import core, prefs, visual, sound
 from psychopy.hardware import keyboard
+from psychopy.hardware.emulator import launchScan
 import librosa
 
 
@@ -26,14 +27,14 @@ def run_miniblock(img_stimuli, audio_stimuli, window, n_stimuli=4, stim_len=4.0,
     flip_hook = window.getFutureFlipTime(clock='ptb')
     audio_stim.play(when=flip_hook)
     window.flip()
-    core.wait(stim_len+1.5)
+    core.wait(stim_len)
     # Present the stimuli for the miniblock.
     stim_keys = list(img_stimuli.keys())
     block_stims = sample(stim_keys, n_stimuli)
     for stim in block_stims:
         # Prepare the image stimulus.
-        img_stim = visual.ImageStim(window, image=img_stimuli[stim])
-        img_stim.size = (1.2, 1.4) # NOTE: this looks approximately right to me, but may need to be adjusted.
+        img_stim = visual.ImageStim(window, image=img_stimuli[stim], size=(2.0, 2.0))
+        #img_stim.size = (1.2, 1.2) # NOTE: this looks approximately right to me, but may need to be adjusted.
         img_stim.draw()
         # Prepare the audio stimulus.
         audio_vec = audio_stimuli[stim][0].reshape(-1,1)
@@ -49,7 +50,7 @@ def run_miniblock(img_stimuli, audio_stimuli, window, n_stimuli=4, stim_len=4.0,
 
 if __name__=='__main__':
     # Create a window
-    win = visual.Window([400,300])
+    win = visual.Window([400,400])
  
     # Needed to make the audio actually play...
     sound.setDevice(dev=0, kind='output')
@@ -65,21 +66,23 @@ if __name__=='__main__':
     face_audio = {path.split('/')[-1].split('.wav')[0].replace('_', ' '): librosa.load(path, sr=48000) for path in face_audio_paths}
     letter_audio = {path.split('/')[-1].split('.wav')[0].replace('_', ' '): librosa.load(path, sr=48000) for path in letter_audio_paths}
 
+    #### TEST SCANNER SYNCING ####
+    MR_settings = {'TR': 2.0, 'sync': 'equal', 'volumes': 10.0, 'skip': 0.0}
+    launchScan(win, MR_settings, mode='test', wait_msg='Waiting for pulse.')
     key_resp = keyboard.Keyboard()
     while True:
-        resp = key_resp.get_keys(key_list=['equal'], waitRelease=False)
-        if len(resp):
-            print(resp)
+        allKeys = key_resp.getKeys(keyList=['equal', '5'], waitRelease=False)
+        if MR_settings['sync'] in allKeys:
             break
 
 
     # Run the experiment.
     n_blocks = 1
     for _ in range(n_blocks):
-        win = run_miniblock(face_imgs, face_audio, win, n_stimuli=4, stim_len=1.5, cond='open')
-        win = run_miniblock(face_imgs, face_audio, win, n_stimuli=4, stim_len=1.5, cond='closed')
-        win = run_miniblock(letter_imgs, letter_audio, win, n_stimuli=4, stim_len=1.5, cond='open')
-        win = run_miniblock(letter_imgs, letter_audio, win, n_stimuli=4, stim_len=1.5, cond='closed')
+        win = run_miniblock(face_imgs, face_audio, win, n_stimuli=4, stim_len=3.0, cond='open')
+        win = run_miniblock(face_imgs, face_audio, win, n_stimuli=4, stim_len=3.0, cond='closed')
+        win = run_miniblock(letter_imgs, letter_audio, win, n_stimuli=4, stim_len=3.0, cond='open')
+        win = run_miniblock(letter_imgs, letter_audio, win, n_stimuli=4, stim_len=3.0, cond='closed')
         
         # Add a fixation period
         message = visual.TextStim(win, text='+')
